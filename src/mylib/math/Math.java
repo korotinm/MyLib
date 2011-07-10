d58ede2fffde37106250dc12afd665251c26137d
korotin <korotinm@gmail.com>
2011-07-11 00:21:28 +0400

    dist levenshstein and method Math.min

---
+++ src/mylib/math/Math.java
@@ -0,0 +1,27 @@
+package mylib.math;
+
+/**
+ * @author M.Korotin
+ *         Date: 10.07.11
+ *         Time: 23:12
+ */
+public class Math {
+
+    public static int min(int a, int b){
+        return a < b ? a : b;
+    }
+
+    public static int min(int a, int b, int c){
+        if(a < b){
+            if(a < c){
+                return a;
+            }
+        }else{
+            if(b < c){
+                return b;
+            }
+        }
+        return c;
+    }
+
+}
